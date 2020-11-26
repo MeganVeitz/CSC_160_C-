806038a029c35bcf2de58852b999768c133db169
MeganVeitz <megan.veitz@gmail.com>
2020-11-25 17:39:34 -0700

    skeleton for user input and then display it back.

---
+++ getline_example.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string line;
+
+int main( )
+{
+   cout << "Enter a line of input \n";
+   getline(cin, line);
+   cout << line << " End of output \n";
+   
+return 0;
+}
